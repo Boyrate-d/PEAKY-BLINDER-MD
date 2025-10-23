fdd0fa7e8478edaf5bb70ed91f1439fc4348e54b
Boyrate-d <nevillekimutai327@gmail.com>
2025-10-23 23:57:48 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with blinder~
-  OWNER_NUMBER: "254116266407", // PUT OWNER NUMBER HERE
+  SESSION_ID: "blinder~EYJV2IhS#hgr_16XajqpjX20t36caNGkL_Thj_X7SKdA-ZQOU6vQ ",  // add your Session Id make sure it starts with blinder~
+  OWNER_NUMBER: "254787943022", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
