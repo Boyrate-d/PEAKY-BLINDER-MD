e4ca3ecb4b8f242bdc06f9dddb4cdf310c174817
Boyrate-d <nevillekimutai327@gmail.com>
2025-10-29 21:18:10 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "blinder~sAwTQLxL#-Sl-JRF17-wE_o960U6Uui6NPGGrAoOkH963uBG9pj4",  // add your Session Id make sure it starts with blinder~
+  SESSION_ID: "blinder~tEgjBB6Z#-lYsBqr0Hrgrhq3YxEZL1b8w87RwN5dID0dXl_RWqUw",  // add your Session Id make sure it starts with blinder~
   OWNER_NUMBER: "254787943022", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
